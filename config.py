5f471eb4d374f2d76a2075835b15be65900f3782
henolazues <125171473+henolazues@users.noreply.github.com>
2023-02-12 09:43:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,1 @@
-USER_AGENT = "tdr01dV4"
+USER_AGENT = "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; BOLT/2.340) AppleWebKit/530+ (KHTML, like Gecko) Version/4.0 Safari/530.17 UNTRUSTED/1.0 3gpp-gba"
